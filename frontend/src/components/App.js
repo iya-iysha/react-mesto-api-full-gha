b1b65ad14940425883a4b13643b5c737f5dba207
Iya Vaksina <iya-iysha@mail.ru>
2023-08-03 15:11:35 +0300

    fix currentUser

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -159,7 +159,7 @@ function App() {
 
   React.useEffect(() => {
     handleTokenCheck();
-  }, [loggedIn, currentUser])
+  }, [])
 
   React.useEffect(() => {
     api.getUserInfo()
@@ -167,7 +167,7 @@ function App() {
       setCurrentUser(data.data);
     })
     .catch((err) => console.log(err));
-  }, [loggedIn, currentUser])
+  }, [])
 
   React.useEffect(() => {
     api.getCardsInfo()
